b736f9936aae560d223e6aac7529d1693bfff709
hablu <131282586+hablu@users.noreply.github.com>
2024-02-25 13:00:29 +0600

    customer data create

---
+++ public/customerData.js
@@ -0,0 +1,24 @@
+const data = [
+  {
+    img: "/man.png",
+    number: 360452,
+    title: "Projects",
+  },
+  {
+    img: "/Like.png",
+    number: 25000,
+    title: "Happy Customers",
+  },
+  {
+    img: "/Happy  1.png",
+    number: 6300,
+    title: "Solved Problems",
+  },
+  {
+    img: "/Scan 3.png",
+    number: 240452,
+    title: "Followers",
+  },
+];
+
+
