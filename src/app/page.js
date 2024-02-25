9bb78cb43f020c6fead481f3bb5174994aa36ddf
hablu <131282586+hablu@users.noreply.github.com>
2024-02-25 12:24:19 +0600

    sub com create

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import Subscribe from '@/components/Subscribe';
 import React from 'react';
 
 const page = () => {
   return (
     <div>
-      <h1>hello</h1>
+      <Subscribe />
     </div>
   );
 };
