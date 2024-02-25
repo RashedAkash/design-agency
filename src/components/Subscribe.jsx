e18a2c7edc9b0b710c35563d3c41668f3f3431fa
hablu <131282586+hablu@users.noreply.github.com>
2024-02-25 12:46:01 +0600

    subscribe complete

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -12,25 +12,33 @@ const Subscribe = () => {
             <div className="flex flex-col w-full ">
               <h1 className="text-xl poppins text-[#20B15A] font-medium text-center ">
                 SUBSCRIBE
-                
               </h1>
 
-              <p className="max-w-3xl mx-auto mt-6 text-lg text-center text-gray-700 dark:text-white md:text-xl">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
-                alias nihil incidunt.
+              <p className="capitalize text-center text-[#000] font-semibold  text-[30px] poppins">
+                Subscribe to get the latest <br /> news about us
+              </p>
+              <p className=" font-sans font-medium text-[#8B8B8B] capitalize text-center">
+                Please drop your email below to get daily update about what we
+                do
               </p>
 
-              <div className="flex flex-col mt-8 space-y-3 sm:-mx-2 sm:flex-row sm:justify-center sm:space-y-0">
-                <input
-                  id="email"
-                  type="text"
-                  className="px-6 py-3 text-gray-700 bg-white border rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring sm:mx-2"
-                  placeholder="Email Address"
-                />
+              <div className="  flex justify-center">
+                <div class="w-full   mt-8 bg-transparent border rounded-lg lg:max-w-sm dark:border-gray-700 focus-within:border-[#000] focus-within:ring focus-within:ring-blue-300  focus-within:ring-opacity-40">
+                  <form class="flex flex-col lg:flex-row">
+                    <input
+                      type="email"
+                      placeholder="Enter your email address"
+                      class="flex-1 capitalize h-10 px-4 py-2 m-1 text-gray-700 placeholder-gray-400 bg-transparent border-none appearance-none dark:text-gray-200 focus:outline-none focus:placeholder-transparent focus:ring-0"
+                    />
 
-                <button className="px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-md hover:bg-blue-600 focus:bg-blue-600 focus:outline-none sm:mx-2">
-                  Notify Me
-                </button>
+                    <button
+                      type="button"
+                      class="h-10 px-4 py-2 m-1 text-white transition-colors duration-300 transform   hover:bg-blue-400 focus:outline-none bg-[#F55F1D] rounded-lg"
+                    >
+                      Subscribe
+                    </button>
+                  </form>
+                </div>
               </div>
 
               <p className="mt-8 text-center text-gray-700 dark:text-white text-md md:text-xl">
