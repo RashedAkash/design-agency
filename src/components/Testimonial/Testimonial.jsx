fac8ba735615b0551545f45b5c1c971b87002cf5
hablu <131282586+hablu@users.noreply.github.com>
2024-02-26 18:02:33 +0600

    test card create

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import { testimonialData } from '../../../public/TestimonialData';
 import SectionTitle from '../SectionTitle';
+import TestimonialCard from './TestimonialCard';
 
 const Testimonial = () => {
   return (
@@ -10,8 +11,10 @@ const Testimonial = () => {
           title="Testimonial List"
         />
       </div>
-      <div>
-
+      <div className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
+        {
+          testimonialData?.map(test=> <TestimonialCard key={test?.des} test={test} />)
+}
       </div>
     </div>
   );
--- src/components/Testimonial/TestimonialCard.jsx
+++ src/components/Testimonial/TestimonialCard.jsx
@@ -1,9 +1,21 @@
+import Image from 'next/image';
 import React from 'react';
 
-const TestimonialCard = () => {
+const TestimonialCard = ({test}) => {
   return (
     <div>
-      
+      <div className="card  bg-white shadow-xl">
+        <figure className="px-10 pt-10">
+          <Image src={test?.img} alt="img" width={100} height={100} />
+        </figure>
+        <div className="card-body items-center text-center">
+          <p className=" text-[#9D9D9D]">{test?.des}</p>
+          <h1 className=' mt-2 text-[26px] font-semibold text-[#000]'>{test?.name }</h1>
+          <div className="card-actions">
+            <h4 className=' text-[#000]'>{test?.title }</h4>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
