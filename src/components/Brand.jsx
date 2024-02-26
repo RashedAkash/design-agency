8d85b288b3807b53f5687e36d7a36466823f6487
hablu <131282586+hablu@users.noreply.github.com>
2024-02-26 08:46:09 +0600

    animation create on brand

--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -1,59 +1,67 @@
 import Image from 'next/image';
-import React from 'react';
+
+import Marquee from 'react-fast-marquee';
 
 const Brand = () => {
   return (
-    <div className=" grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-36 py-10">
-      <Image
-        src="/logos_google.png"
-        alt="google"
-        width={88}
-        height={28}
-        style={{
-          width: "auto",
-          height: "auto",
-        }}
-      />
-      <Image
-        src="/Trello-logo-blue 1.png"
-        alt="google"
-        width={88}
-        height={28}
-        style={{
-          width: "auto",
-          height: "auto",
-        }}
-      />
-      <Image
-        src="/logos_monday.png"
-        alt="google"
-        width={88}
-        height={28}
-        style={{
-          width: "auto",
-          height: "auto",
-        }}
-      />
-      <Image
-        src="/Notion.png"
-        alt="google"
-        width={88}
-        height={28}
-        style={{
-          width: "auto",
-          height: "auto",
-        }}
-      />
-      <Image
-        src="/Slack.png"
-        alt="google"
-        width={88}
-        height={28}
-        style={{
-          width: "auto",
-          height: "auto",
-        }}
-      />
+    <div className=" py-7 ">
+      <Marquee>
+        <Image
+          src="/logos_google.png"
+          alt="google"
+          width={88}
+          height={28}
+          style={{
+            width: "auto",
+            height: "auto",
+          }}
+          className=" mr-36"
+        />
+        <Image
+          src="/Trello-logo-blue 1.png"
+          alt="google"
+          width={88}
+          height={28}
+          style={{
+            width: "auto",
+            height: "auto",
+          }}
+          className=" mr-36"
+        />
+        <Image
+          src="/logos_monday.png"
+          alt="google"
+          width={88}
+          height={28}
+          style={{
+            width: "auto",
+            height: "auto",
+          }}
+          className=" mr-36"
+        />
+        <Image
+          src="/Notion.png"
+          alt="google"
+          width={88}
+          height={28}
+          style={{
+            width: "auto",
+            height: "auto",
+          }}
+          className=" mr-36"
+        />
+        <Image
+          src="/Slack.png"
+          alt="google"
+          width={88}
+          height={28}
+          style={{
+            width: "auto",
+            height: "auto",
+          }}
+          className=" mr-36"
+        />
+      </Marquee>
     </div>
   );
 };
