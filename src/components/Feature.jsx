3cc0301ef3e85373eecf683f776d5ce8ad1058dd
hablu <131282586+hablu@users.noreply.github.com>
2024-02-26 10:17:18 +0600

    featured design complete

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -10,20 +10,22 @@ const Feature = () => {
           <div className="items-center lg:flex">
             <div className="w-full lg:w-1/2">
               <div className="lg:max-w-lg">
-                <div className="flex flex-col mt-6 space-y-3 ">
+                <div className="flex  flex-col  ">
                   <Image src="/Mask.png" alt="m" width={600} height={530} />
                   <p className=" text-[#2E3E5C] text-[14px] mt-4">
                     App Design - June 20, 2022
                   </p>
-                  <h1 className=" text-[#1E1F4B] font-bold text-2xl">App Redesign</h1>
+                  <h1 className=" text-[#1E1F4B] font-bold text-2xl mt-3">
+                    App Redesign
+                  </h1>
                 </div>
               </div>
             </div>
 
-            <div className="flex items-center justify-center w-full mt-6 lg:mt-0 lg:w-1/2">
-              {
-               featureData?.map(fd=> <FeatureCard fd={fd} key={fd?.img} />) 
-            }
+            <div className="grid grid-cols-1 mt-6 md:grid-cols-2 gap-7  w-full   lg:mt-0 lg:w-1/2">
+              {featureData?.map((fd) => (
+                <FeatureCard fd={fd} key={fd?.img} />
+              ))}
             </div>
           </div>
         </div>
--- src/components/FeatureCard.jsx
+++ src/components/FeatureCard.jsx
@@ -1,9 +1,14 @@
+import Image from 'next/image';
 import React from 'react';
 
 const FeatureCard = ({fd}) => {
   return (
     <div>
-      
+      <div className=" ">
+        <Image src={fd?.img} alt="m" width={287} height={188} />
+        <p className=" text-[#2E3E5C] text-[14px] mt-4">{fd?.title}</p>
+        <h1 className=" text-[#2E3E5C] font-bold text-[18px]">{fd?.text}</h1>
+      </div>
     </div>
   );
 };
