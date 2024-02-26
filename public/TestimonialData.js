ab3a0a5b8e11d8a06a9b6469c634a152484515a5
hablu <131282586+hablu@users.noreply.github.com>
2024-02-26 17:44:54 +0600

    test data create

---
+++ public/TestimonialData.js
@@ -0,0 +1,38 @@
+export const testimonialData = [
+  {
+    img: "/test.png",
+    name: "John Doe",
+    title: "Software Engineer",
+    des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque et placerat metus. Morbi aliquet felis sit amet erat finibus, ac condimentum ligula ornare.",
+  },
+  {
+    img: "/test1.png",
+    name: "Jane Smith",
+    title: "Graphic Designer",
+    des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque et placerat metus. Morbi aliquet felis sit amet erat finibus, ac condimentum ligula ornare.",
+  },
+  {
+    img: "/test2.png",
+    name: "Michael Johnson",
+    title: "Data Scientist",
+    des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque et placerat metus. Morbi aliquet felis sit amet erat finibus, ac condimentum ligula ornare.",
+  },
+  {
+    img: "/test3.png",
+    name: "Emily Brown",
+    title: "Marketing Manager",
+    des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque et placerat metus. Morbi aliquet felis sit amet erat finibus, ac condimentum ligula ornare.",
+  },
+  {
+    img: "/test4.png",
+    name: "Alex Davis",
+    title: "UX/UI Designer",
+    des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque et placerat metus. Morbi aliquet felis sit amet erat finibus, ac condimentum ligula ornare..",
+  },
+  {
+    img: "/test5.png",
+    name: "Sarah Wilson",
+    title: "Project Manager",
+    des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque et placerat metus. Morbi aliquet felis sit amet erat finibus, ac condimentum ligula ornare.",
+  },
+];
